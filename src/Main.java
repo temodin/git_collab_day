5337d34152b4b4277b64076bf346425f33f05052
Pal_Dorka <pal.dorka.91@gmail.com>
2021-02-15 15:10:55 +0100

    b1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Hello Dorka, csa Zoli");
+        count();
     }
+
+    public static void count(){
+        System.out.println("Our age is " + (35+33+29));
+    }
+
+
+
 }
