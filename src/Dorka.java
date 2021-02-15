351aa1259849da1cf31101e7a4f6ad32b0c8b79b
temodin <solyom.balint@gmail.com>
2021-02-15 15:46:20 +0100

    a3

--- src/Dorka.java
+++ src/Dorka.java
@@ -1,6 +1,5 @@
 public class Dorka {
-    public void count(Integer input){
-        System.out.println("Our age is " + (35+33+29));
-        System.out.println(input);
+    public void count(){
+        System.out.println("I was born in month 2 ");
     }
 }
