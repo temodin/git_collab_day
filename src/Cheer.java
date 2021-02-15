9ade2ac3bfbe054c2f8dc6c2933abebc69e9b871
zokeleti <keletizoltan0229@gmail.com>
2021-02-15 15:16:28 +0100

    c1

---
+++ src/Cheer.java
@@ -0,0 +1,5 @@
+public class Cheer {
+    public void cheer(){
+        System.out.println("Kacsak");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Kacsák");
 
         System.out.println("Hello Dorka, csa Zoli");
     }
