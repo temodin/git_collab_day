5ac775dc6b7a37db2756c3ddebe98226213f8fb2
Pal_Dorka <pal.dorka.91@gmail.com>
2021-02-15 15:32:39 +0100

    added parameter requirement

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Cheer cheer = new Cheer();
         System.out.println("Hello Dorka, csa Zoli");
         balint.printName();
-        dorka.count();
+        dorka.count(5);
         cheer.cheer();
     }
 }
