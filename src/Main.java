75e8f334c5d1e0fee8b8a77d2cee7b49275852a4
temodin <solyom.balint@gmail.com>
2021-02-15 15:10:56 +0100

    a1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Dorka, csa Zoli");
+        printName();
+
+    }
+
+    public static void printName() {
+        System.out.println("Balintka");
     }
 }
