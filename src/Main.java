c22402d0e1ed9d1e5237c3bbfe44014ab2e97d81
zokeleti <keletizoltan0229@gmail.com>
2021-02-15 15:50:23 +0100

    c4

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,12 @@ public class Main {
         Dorka dorka = new Dorka();
         Cheer cheer = new Cheer();
         System.out.println("Hello Dorka, csa Zoli");
-<<<<<<< HEAD
         balint.printName();
-        dorka.count(5);
-=======
-        balint.printName("Gittes");
+
         dorka.count();
 
 
->>>>>>> 3e690e79dcc90c91f13a09e986cda279dfc8f48c
+
         cheer.cheer("Szia Világ!");
 
     }
