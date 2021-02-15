fec7f0f61e06fa5d89ec24223a7a1eaf1bdbadc0
Pal_Dorka <pal.dorka.91@gmail.com>
2021-02-15 16:19:07 +0100

    b6

--- src/Cheer.java
+++ src/Cheer.java
@@ -1,6 +1,6 @@
 public class Cheer {
 
     public void cheer(String wordToShout, String otherWordToShout){
-        System.out.println(wordToShout + " + " + otherWordToShout);
+        System.out.println("Greetings for the " + wordToShout);
     }
 }
